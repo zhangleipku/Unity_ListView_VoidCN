4b9359178a88463a31ec09e4489670c71a15b8c5
Zhang Lei <zhanglei.cs.pku@gmail.com>
2016-08-29 10:46:03 +0900

    Generate list element by C# script

---
+++ Assets/ListViewController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ListViewController : MonoBehaviour {
+
+	[SerializeField]
+	RectTransform prefab = null;
+
+	void Start () 
+	{
+		for(int i=0; i<5; i++)
+		{
+			var item = GameObject.Instantiate(prefab) as RectTransform;
+			item.SetParent(transform, false);
+
+//			var text = item.GetComponentInChildren<Text>();
+//			text.text = "item:" + i.ToString();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
