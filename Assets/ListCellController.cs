abffb754bfcc48ae756f874dde0cc5129bd343b5
Zhang Lei <zhanglei.cs.pku@gmail.com>
2016-08-29 14:26:16 +0900

    Prefab can be modified by script

---
+++ Assets/ListCellController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ListCellController : MonoBehaviour {
+
+	public Text lblTitle;
+	public Text lblBody;
+	public Button btnOK;
+	public Image imgIcon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		lblTitle.text = System.DateTime.Now.ToString ();
+	}
+}
--- Assets/ListViewController.cs
+++ Assets/ListViewController.cs
@@ -4,22 +4,39 @@ using System.Collections;
 public class ListViewController : MonoBehaviour {
 
 	[SerializeField]
-	RectTransform prefab = null;
+//	Transform prefab = null;
+	GameObject prefab = null;
 
 	void Start () 
 	{
 		for(int i=0; i<5; i++)
 		{
-			var item = GameObject.Instantiate(prefab) as RectTransform;
-			item.SetParent(transform, false);
+			GameObject instance = (GameObject)Instantiate(prefab);
+
+			instance.transform.SetParent(transform, false);
+
+			instance.tag = "ListCell";
+
+			ListCellController lcc = instance.GetComponentInChildren<ListCellController> ();
+
+			if (lcc) {
+				lcc.lblBody.text = i.ToString ();
+			}
 
 //			var text = item.GetComponentInChildren<Text>();
 //			text.text = "item:" + i.ToString();
+
+			//GameObject.gameObject.tag="Player";
+			//gameObject.tag should be enough.
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		var a = this.GetComponentsInChildren<RectTransform>();
+
+		var b = GameObject.FindGameObjectsWithTag ("ListCell");
 
 	}
 }
